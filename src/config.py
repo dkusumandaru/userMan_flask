2c67dea0d5476bc03a2e3bb042e956ba3b55e949
dkusumandaru <darmakusumandaru@gmail.com>
2022-12-15 10:27:15 +0700

    update consume api auth

--- src/config.py
+++ src/config.py
@@ -2,7 +2,7 @@ import os
 
 class Package():
 	_data = {}
-	_data['name'] = 'UserMan'
+	_data['name'] = 'usrMan'
 	_data['version'] = '1.0.0'
 	_data['release'] = '2022-11-12'
 	_data['author'] = 'Ar'
@@ -18,7 +18,15 @@ class Config():
 class Security():
 	_data = {}
 	_data['salt'] = 'fly me to the moon'
-	_data['secret_key'] = '<Hall0 W0rld>'
+	_data['secret_key'] = 'Hall0W0rld'
+
+
+class API():
+	_auth = "http://localhost:5000/auth"
+	_auth_content = {"username":"","password":"","application":""}
+	_auth_header = {"x-access-tokens":"", "Content-Type":"application/json"}
+	_login = "http://localhost:5000/login"
+	_login_header = ""
 
 # print(Config._db)
 # print(Config._db['uname'])
--- src/controllers/authController.py
+++ src/controllers/authController.py
@@ -4,12 +4,85 @@ from app import app
 import os, sys
 sys.path.append('../')
 from systems import package
+from systems import gateway
+from config import API
 
-@app.route('/coffearea', methods=['GET'])
-def coffearea():
 
+@app.route('/coffeearea', methods=['GET'])
+def coffeearea():
+    
     session['AppName'] = package.Package._name
     session['AppVersion'] = package.Package._version
+    
+    # print(session['userid'])
+    userLogAccess = session.get('userid', 'FALSE')
+	
+    if userLogAccess != 'FALSE':
+         return redirect(url_for('dashboard'))
+    else:
+        return render_template("auth/index.html")
 
-    # result = App.query.filter_by(is_deleted=False).all()
-    return render_template("auth/index.html")
+
+@app.route('/dashboard', methods=['GET'])
+def dashboard():
+    session['AppName'] = package.Package._name
+    session['AppVersion'] = package.Package._version
+    userLogAccess = session.get('userid', 'FALSE')
+	
+    if userLogAccess != 'FALSE':
+        return render_template("dashboard/index.html")
+    else:
+        return redirect(url_for('coffeearea'))
+
+
+@app.route('/espresso', methods=['POST'])
+def espresso():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        loginUrl = API._login
+        apiUrl = API._auth
+        contents = API._auth_content
+        headers = API._auth_header
+
+        contents['username'] = email
+        contents['password'] = password
+        contents['application'] = session['AppName']
+
+        # resp = requests.post(url, data={}, auth=('user', 'pass'))
+        print("hello")
+        try:
+            print("hello 1")
+            getToken = gateway.Gateway.auth(loginUrl, {}, (email, password))
+
+            print(getToken)
+            print(getToken["token"])
+
+            print(app.secret_key+" "+getToken["token"])
+            headers['x-access-tokens'] = app.secret_key+" "+getToken["token"]
+            getAccess = gateway.Gateway.post(apiUrl, contents, headers)
+
+            print(getAccess["data"]["access"])
+    
+            if getAccess["data"]["access"] == True :
+                # print("here")
+                session['userid'] = getAccess["data"]["id"]
+                session['username'] = getAccess["data"]["username"]
+                session['role'] = getAccess["data"]["role"]
+                session['application_id'] = getAccess["data"]["application_id"]
+                session['role_id'] = getAccess["data"]["role_id"]
+                session['access'] = getAccess["data"]["access"]
+
+                return redirect(url_for('dashboard'))
+            else:
+                return redirect(url_for('coffeearea'))
+                flash('Login Failed...')
+        except Exception as e:
+            flash('Login Failed...')
+            return redirect(url_for('coffeearea'))
+
+
+@app.route('/logout')
+def logout():
+	session.clear()
+	return redirect('coffeearea')
